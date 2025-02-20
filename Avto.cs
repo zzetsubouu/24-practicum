492b0aa1e8fe2fa62f68fb254dca15c8bf004a17
Kab-35-16 <Kab-35-16@KAB-31-8->
2025-02-20 10:34:51 +0200

    1 задание

---
+++ Avto.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_practicum
+{
+    class Avto
+    {
+        private string brand;
+        private string color;
+        private int skor;
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                brand = value;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        public int Skor
+        {
+            get
+            {
+                return skor;
+            }
+            set
+            {
+                if (value < 20)
+                {
+                    skor = 20;
+                }
+                else if (value > 120)
+                {
+                    skor = 120;
+                }
+                else
+                {
+                    skor = value;
+                }
+            }
+        }
+        public Avto()
+        {
+
+        }
+        public Avto(string brand, string color, int skor)
+        {
+            this.brand = brand;
+            this.color = color;
+            this.skor = skor;
+        }
+        public override string ToString()
+        {
+            return $"марка: {brand}, цвет: {color}, скорость: {skor}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace _24_practicum
     {
         static void Main(string[] args)
         {
+            var avto1 = new Avto();
+            avto1.Brand = "toyota";
+            avto1.Color = "Red";
+            avto1.Skor = 60;
+            var avto2 = new Avto("Volkswagen", "Blue", 50);
+
+            Console.WriteLine(avto1.ToString());
+            Console.WriteLine(avto2.ToString());
+
+            Console.Write("введите марку авто: ");
+            string userBrand = Console.ReadLine();
+            Console.Write("введите цвет авто: ");
+            string userColor = Console.ReadLine();
+            Console.Write("введите скорость авто: ");
+            int userSkor = int.Parse(Console.ReadLine());
+            var userAvto = new Avto(userBrand, userColor, userSkor);
+            Console.WriteLine(userAvto.ToString());
         }
     }
 }
